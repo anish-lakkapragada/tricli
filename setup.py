62cd3659415568d2d711a0f1ae3bbc73ca89d087
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-04-27 22:02:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     version = '2.0.0',
     author = "Anish Lakkapragada", 
     author_email="anish.lakkapragada@gmail.com",
-    description = "tricli manages a trie data structure that can be globally accessed."
-    keywords=["REST", "Data Structures", "Trie"]
+    description = "tricli manages a trie data structure that can be globally accessed.", 
+    keywords=["REST", "Data Structures", "Trie"], 
     packages = ['tricli'],
     license = "MIT", 
     entry_points = {
