4c5ade2ad913b504603b867da8deb29a1c2cac1a
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-04-27 22:03:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+
+version_name = sys.argv[1].replace("refs/tags/", "")
+del sys.argv[1]
+
+
 setup(
     name = 'tricli',
     version = '2.0.0',
     author = "Anish Lakkapragada", 
     author_email="anish.lakkapragada@gmail.com",
     description = "tricli manages a trie data structure that can be globally accessed.", 
+    version=version_name, 
     keywords=["REST", "Data Structures", "Trie"], 
     packages = ['tricli'],
     license = "MIT", 
